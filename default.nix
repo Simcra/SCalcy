f8b8120f489272eebc4b3cf2dbf61858f43e9d50
Simcra <5228381+Simcra@users.noreply.github.com>
2024-08-29 15:40:58 +0930

    Update slint version in Cargo.toml, switch from using crane to nixpkgs rust platform

---
+++ default.nix
@@ -0,0 +1,60 @@
+{ lib
+, stdenv
+, rustPlatform
+, fetchFromGitHub
+, makeWrapper
+, pkg-config
+, openssl
+, libxkbcommon
+, fontconfig
+, wayland
+, xorg ? null
+, ...
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "scalcy";
+  version = "0.1.2";
+
+  src = fetchFromGitHub {
+    owner = "simcra";
+    repo = "scalcy";
+    rev = "v${version}";
+    hash = "sha256-I6p0Cjj52waOCR1wUIlHUmXsCd9f/CKL2To/sakDyYI=";
+  };
+
+  buildInputs = lib.optionals stdenv.isLinux [
+    libxkbcommon
+    fontconfig
+
+    # Wayland
+    wayland
+
+    # Xorg/X11
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+  ];
+  nativeBuildInputs = lib.optionals stdenv.isLinux [
+    pkg-config
+    openssl
+    makeWrapper
+  ];
+
+  postInstall = lib.optionalString stdenv.isLinux ''
+    wrapProgram $out/bin/scalcy --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}"
+  '';
+
+  cargoLock = {
+    lockFile = src + /Cargo.lock;
+  };
+
+  meta = {
+    mainProgram = pname;
+    description = "A simple calculator built using Slint UI with the Rust programming language";
+    homepage = "https://github.com/simcra/scalcy";
+    changelog = "https://github.com/simcra/scalcy/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,126 +1,42 @@
 {
-  description = "Build the cargo project while also compiling the standard library";
+  description = "Scalcy is a simple calculator built using Slint UI with the Rust programming language";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-parts, ... } @ inputs:
+  flake-parts.lib.mkFlake {inherit inputs; } {
+    systems = nixpkgs.lib.systems.flakeExposed;
+    perSystem = {pkgs, ...}: 
       let
-        pname = "scalcy";
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-        inherit (pkgs) lib;
-
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-        };
-
-        # NB: we don't need to overlay our custom toolchain for the *entire*
-        # pkgs (which would require rebuidling anything else which uses rust).
-        # Instead, we just want to update the scope that crane will use by appending
-        # our specific toolchain there.
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-        # Clean the source with slint files as well as the default filterCargoSources
-        src = lib.cleanSourceWith {
-          src = ./.;
-          filter = path: type:
-            (lib.hasSuffix "\.slint" path) ||
-            (craneLib.filterCargoSources path type);
-        };
-
-        libPath = with pkgs; lib.makeLibraryPath [
-          wayland
-          libxkbcommon
-          fontconfig
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-        ];
-
-        commonArgs = {
-          inherit src pname;
-          strictDeps = true;
-
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            openssl
-            makeWrapper
-          ];
-        };
-
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        crate = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-
-          cargoVendorDir = craneLib.vendorMultipleCargoDeps {
-            inherit (craneLib.findCargoFiles src) cargoConfigs;
-            cargoLockList = [
-              ./Cargo.lock
-
-              # Unfortunately this approach requires IFD (import-from-derivation)
-              # otherwise Nix will refuse to read the Cargo.lock from our toolchain
-              # (unless we build with `--impure`).
-              #
-              # Another way around this is to manually copy the rustlib `Cargo.lock`
-              # to the repo and import it with `./path/to/rustlib/Cargo.lock` which
-              # will avoid IFD entirely but will require manually keeping the file
-              # up to date!
-              "${rustToolchain.passthru.availableComponents.rust-src}/lib/rustlib/src/rust/Cargo.lock"
-            ];
-          };
-
-          postInstall = ''
-            wrapProgram $out/bin/${pname} --prefix LD_LIBRARY_PATH : ${libPath}
-          '';
-        });
+        scalcyPkg = pkgs.callPackage ./default.nix { };
+        scalcyBin = nixpkgs.lib.getBin scalcyPkg;
       in
       {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit crate;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          crate-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
-
-          # Check formatting
-          crate-cargofmt = craneLib.cargoFmt { inherit src; };
+        packages = rec {
+          scalcy = scalcyPkg;
+          default = scalcy;
         };
 
-        packages.default = crate;
-
-        apps.default = flake-utils.lib.mkApp { drv = crate; };
-
-        devShells.default = craneLib.devShell {
-          inputsFrom = builtins.attrValues self.checks;
-          LD_LIBRARY_PATH = libPath;
+        apps = rec {
+          scalcy = {
+            type = "app";
+            program = "${scalcyBin}/bin/scalcy";
+          };
+          default = scalcy;
         };
-      }
-    );
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells = rec {
+          scalcy = pkgs.mkShell {
+            buildInputs = scalcyPkg.buildInputs;
+            nativeBuildInputs = scalcyPkg.nativeBuildInputs;
+          };
+          default = scalcy;
+        };
+      };
+    };
 }
